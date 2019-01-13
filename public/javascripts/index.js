4e67ba656ec4d735871ccb97450454ad139656df
Sameeh <sameehkhan1@gmail.com>
2019-01-12 23:47:46 -0800

    Refactor

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,4 @@
 import NBA from 'nba';
-import { SIGBREAK } from 'constants';
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -19,77 +18,30 @@ document.addEventListener('DOMContentLoaded', () => {
     let playerStats = [];
 
     NBA.stats.playerStats().then( res => { 
-        stats = res.leagueDashPlayerStats;
-        console.log(stats)
-        stats.forEach( player => {
-            if( player.pts > 20){
-            playerStats.push(player.pts)
-            }
-        });
+        let stats = res.leagueDashPlayerStats;
+        console.log(stats);
+        // stats.forEach( player => {
+        //     if( player.pts > 20){
+        //     playerStats.push(player.pts)
+        //     }
+        // });
 
         let pstats = playerStats.sort().reverse().slice(0,15);
 
-        var svgWidth = 500, svgHeight = 300, barPadding = 5;
-        var barWidth = (svgWidth / pstats.length)
-
-        var svg = d3.select("body").append("svg")
-            .attr("height", "80%")
-            .attr("width", "80%");
-
-        var yScale = d3.scaleLinear()
-            .domain([0, d3.max(pstats)])
-            .range([svgHeight, 0]);
 
 
-        var barChart = svg.selectAll("rect")
-            .data(pstats)
-            .enter()
-            .append("rect")
-            .attr("y", function (d) {
-                return svgHeight - d
-            })
-            .attr("height", function (d) {
-                return d
-            })
-            .attr("width", barWidth - barPadding)
-            .attr("transform", function (d, i) {
-                var translate = [barWidth * i, 0];
-                return "translate(" + translate + ")";
-            });
 
-
-        // svg.selectAll("rect")
-        //     .data(pstats)
-        //     .enter().append("rect")
-        //         .attr("height", function (d, i) { return (d * 10) })
-        //         .attr("width", "20")
-        //         .attr("class", "bar")
-        //         .attr("x", function (d, i) { return (i * 40) + 20; })
-        //         .attr("y", function (d, i) { return 400 - (d * 10) });
     
-
-
-
-        // var y_axis = d3.axisLeft().scale(yScale);
-
-        // svg.append("g")
-        //     .attr("transform", "translate(20, 100)")
-        //     .call(y_axis);
-
-
         
             
-    });
+    }).catch(function(error){
+        console.log(error);
+    })
 
 
     // let lakers = NBA.teamIdFromName('Los Angeles Lakers')
     // console.log(lakers)
 
-
-
-    // d3.select('h1').style('color', 'red')
-    //     .attr('class', 'heading')
-    //     .text('Hello D3');
 
     // let search = NBA.searchPlayers('james')
     // console.log(search);
@@ -139,14 +91,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // NOT WORKING
     // playerClutch
 
-
-
-    // 0: "teamIdFromName"
-    // 1: "playerIdFromName"
-    // 2: "findPlayer" Returns first player with name match (first in array of James' i.e.)
-    // 3: "searchPlayers"  You can search by player's name
-    // 4: "updatePlayers" idk 
-    // 5: "updateTeams" idk 
-
     
 })
+
