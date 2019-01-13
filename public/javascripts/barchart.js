512077e69349e48532e86adf181bc42ad57a8aed
Sameeh <sameehkhan1@gmail.com>
2019-01-12 17:12:23 -0800

    Bar chart

---
+++ public/javascripts/barchart.js
@@ -0,0 +1,13 @@
+// var dataArray = [23, 13, 21, 14, 37, 15, 18, 34, 30];
+
+// var svg = d3.select("body").append("svg")
+//     .attr("height", "100%")
+//     .attr("width", "100%");
+
+// svg.selectAll("rect")
+//     .data(dataArray)
+//     .enter().append("rect")
+//     .attr("height", function (d, i) { return (d * 10) })
+//     .attr("width", "40")
+//     .attr("x", function (d, i) { return i * 60; })
+//     .attr("y", function (d, i) { return 400 - (d * 10) });
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,5 @@
 import NBA from 'nba';
+import { SIGBREAK } from 'constants';
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -11,25 +12,117 @@ document.addEventListener('DOMContentLoaded', () => {
     //     return typeof NBA[p] === 'function';
     // }));
 
-    let lakers = NBA.teamIdFromName('Los Angeles Lakers')
-    console.log(lakers)
+    // let lebron = NBA.playerIdFromName('Lebron James');
+    // NBA.stats.playerInfo({ PlayerID: lebron }).then(console.log);
 
-    let lebron = NBA.playerIdFromName('Lebron James')
+    let stats; 
+    let playerStats = [];
+
+    NBA.stats.playerStats().then( res => { 
+        stats = res.leagueDashPlayerStats;
+        console.log(stats)
+        stats.forEach( player => {
+            if( player.pts > 20){
+            playerStats.push(player.pts)
+            }
+        });
+
+        let pstats = playerStats.sort().reverse().slice(0,15);
+
+        var svgWidth = 500, svgHeight = 300, barPadding = 5;
+        var barWidth = (svgWidth / pstats.length)
+
+        var svg = d3.select("body").append("svg")
+            .attr("height", "80%")
+            .attr("width", "80%");
+
+
+        var barChart = svg.selectAll("rect")
+            .data(pstats)
+            .enter()
+            .append("rect")
+            .attr("y", function (d) {
+                return svgHeight - d
+            })
+            .attr("height", function (d) {
+                return d
+            })
+            .attr("width", barWidth - barPadding)
+            .attr("transform", function (d, i) {
+                var translate = [barWidth * i, 0];
+                return "translate(" + translate + ")";
+            });
+
+
+        // svg.selectAll("rect")
+        //     .data(pstats)
+        //     .enter().append("rect")
+        //         .attr("height", function (d, i) { return (d * 10) })
+        //         .attr("width", "20")
+        //         .attr("class", "bar")
+        //         .attr("x", function (d, i) { return (i * 40) + 20; })
+        //         .attr("y", function (d, i) { return 400 - (d * 10) });
+    
+
+        // var yScale = d3.scaleLinear()
+        //     .domain([0,d3.max(pstats)])
+        //     .range([svgHeight, 0]);
+
+
+        // var y_axis = d3.axisLeft().scale(yScale);
+
+        // svg.append("g")
+        //     .attr("transform", "translate(20, 100)")
+        //     .call(y_axis);
+
+
+        
+            
+    });
+
+
+    // let lakers = NBA.teamIdFromName('Los Angeles Lakers')
+    // console.log(lakers)
+
+
+
+    // d3.select('h1').style('color', 'red')
+    //     .attr('class', 'heading')
+    //     .text('Hello D3');
 
     // let search = NBA.searchPlayers('james')
     // console.log(search);
 
     // NBA.stats.teamInfoCommon({ TeamID: lakers }).then(console.log);
-    // NBA.stats.playerClutch({ PlayerID: lebron }).then(console.log);
+    // NBA.stats.playerStats({ PlayerID: lebron }).then(console.log);
 
     // let per = "PerGame"
 
     // NBA.stats.team_clutch({TeamID: lakers}).then(console.log);
     // NBA.stats.playerShooting().then(console.log);
     // NBA.stats.teamShooting().then(console.log);
-    NBA.stats.measureType().then(console.log);
+    // NBA.stats.measureType().then(console.log);
 
 
+
+    NBA.stats.teamStats()
+
+
+
+
+
+
+    // let stats;
+    // let teamsStats = [];
+
+    // NBA.stats.playerStats().then(res => {
+    //     stats = res.leagueDashPlayerStats;
+    //     console.log(stats)
+    //     stats.forEach(player => {
+    //         if (player.pts > 20) {
+    //             playerStats.push(player.pts)
+    //         }
+    //     });
 
     // TEAMS
 
